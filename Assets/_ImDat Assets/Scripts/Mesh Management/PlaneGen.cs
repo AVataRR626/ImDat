c0d17709a68160f2a68d1b9e744c96d4c121a06f
AVataRR626 <matt.cabanag@gmail.com>
2025-01-21 19:12:42 +1100

    boop

--- Assets/_ImDat Assets/Scripts/Mesh Management/PlaneGen.cs
+++ Assets/_ImDat Assets/Scripts/Mesh Management/PlaneGen.cs
@@ -19,6 +19,9 @@ public class PlaneGen : MonoBehaviour
     public float xSize = 1.0f;
     public float ySize = 1.0f;
 
+    public Vector3 refVectorA;
+    public Vector3 refVectorB;
+
 
     private void Awake() 
     {
@@ -71,7 +74,8 @@ public class Plane : ProceduralShape
     private void CreateMesh() 
     {
         CreateVertices();
-        CreateTriangles();
+        //CreateTrianglesOneFace();
+        CreateTrianglesTwoFace();
         CreateUVs();
 
         mesh_.vertices = vertices;
@@ -92,7 +96,7 @@ public class Plane : ProceduralShape
             }
         }
     }
-    private void CreateTriangles()
+    private void CreateTrianglesOneFace()
     {
         triangles = new int[3*2*(xCount_ * yCount_ - xCount_ - yCount_ + 1)];
         int triangleVertexCount = 0;
@@ -100,23 +104,64 @@ public class Plane : ProceduralShape
         {
             if(vertex%xCount_ != (xCount_ - 1)) 
             {
+                //--Top Facing--
                 // First triangle
                 int A = vertex;
                 int B = A + xCount_;
                 int C = B + 1;
+                int D = A + 1;
                 triangles[triangleVertexCount] = A;
                 triangles[triangleVertexCount + 1] = B;
                 triangles[triangleVertexCount + 2] = C;
                 //Second triangle
-                B += 1;
-                C = A + 1;
                 triangles[triangleVertexCount + 3] = A;
-                triangles[triangleVertexCount + 4] = B;
-                triangles[triangleVertexCount + 5] = C;
+                triangles[triangleVertexCount + 4] = C;
+                triangles[triangleVertexCount + 5] = D;
                 triangleVertexCount += 6;
+
             }
         }
+    }
 
+    private void CreateTrianglesTwoFace()
+    {
+        triangles = new int[3 * 2 * 2 * (xCount_ * yCount_ - xCount_ - yCount_ + 1)];
+        int triangleVertexCount = 0;
+        for (int vertex = 0; vertex < xCount_ * yCount_ - xCount_; vertex++)
+        {
+            if (vertex % xCount_ != (xCount_ - 1))
+            {
+                //--Top Facing--
+                // First triangle
+                int A = vertex;
+                int B = A + xCount_;
+                int C = B + 1;
+                int D = A + 1;
+                triangles[triangleVertexCount] = A;
+                triangles[triangleVertexCount + 1] = B;
+                triangles[triangleVertexCount + 2] = C;
+
+                //Second triangle
+                triangles[triangleVertexCount + 3] = A;
+                triangles[triangleVertexCount + 4] = C;
+                triangles[triangleVertexCount + 5] = D;
+
+
+                //--Bottom Facing--
+                //First triangle
+                triangles[triangleVertexCount + 6] = A;
+                triangles[triangleVertexCount + 7] = C;
+                triangles[triangleVertexCount + 8] = B;
+
+                //Second triangle
+                triangles[triangleVertexCount + 9] = A;
+                triangles[triangleVertexCount + 10] = D;
+                triangles[triangleVertexCount + 11] = C;
+
+
+                triangleVertexCount += 12;
+            }
+        }
     }
 
     private void CreateUVs() 
@@ -128,5 +173,10 @@ public class Plane : ProceduralShape
             uvs[uvIndexCounter] = new Vector2(vertex.x, vertex.z);
             uvIndexCounter++;
         }
+    }
+
+    public void SetVertex(int x, int y, Vector3 newPos)
+    {
+        vertices[x + y * xCount_] = newPos;
     }
 }
