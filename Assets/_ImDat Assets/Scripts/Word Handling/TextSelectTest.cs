7b3b402bc7267d24e9a00e70b41f8c6986a19b7f
AVataRR626 <matt.cabanag@gmail.com>
2025-07-18 12:30:05 +1000

    Highlight Text Extraction + Unity Upgrade

---
+++ Assets/_ImDat Assets/Scripts/Word Handling/TextSelectTest.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+
+public class TextSelectTest : MonoBehaviour
+{
+    public TMPro.TMP_InputField myInputField;
+    public string selectedText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        myInputField.onTextSelection.AddListener(GetSelectedText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetSelectedText(string str, int start, int end)
+    {
+        selectedText = str.Substring(Mathf.Min(start, end), Mathf.Abs(end - start));
+    }
+}
